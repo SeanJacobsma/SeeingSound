37d817951fb317d3ef241ec5ecfd39236d1b1b3b
SeanJacobsma <sean@jacobsma.net>
2025-11-20 23:27:43 -0500

    Updated composable visuals for Mass, Spring and Damper.

--- app/src/main/java/net/jacobsma/seeingsound/acoustics/composables/Damper.kt
+++ app/src/main/java/net/jacobsma/seeingsound/acoustics/composables/Damper.kt
@@ -1,23 +1,45 @@
 package net.jacobsma.seeingsound.acoustics.composables
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import net.jacobsma.seeingsound.R
+import net.jacobsma.seeingsound.acoustics.damping.EffectiveDamping
 
 @Composable
-fun Damper(leftMassStart: Dp, leftMassAmp: Dp, rightMassStart:Dp, rightMassAmp:Dp) {
+fun Damper(damping: EffectiveDamping, leftMassStart: Dp, leftMassAmp: Dp, rightMassStart:Dp, rightMassAmp:Dp) {
     Box(
         modifier = Modifier
-            .width(25.dp)
-            .offset(0.dp, leftMassStart + leftMassAmp)
-            .height((rightMassStart + rightMassAmp) - (leftMassStart + leftMassAmp))
-            .background(color = Color.Yellow) // This must come after setting offset, otherwise offset is not applied
-    )
+            .offset(y=leftMassStart + leftMassAmp)
+            .height((rightMassStart + rightMassAmp) - (leftMassStart + leftMassAmp)),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(R.drawable.dashpot),
+            contentDescription = "dashpot",
+            modifier = Modifier
+                .width(35.dp)
+                .padding(start = 3.dp, end = 3.dp)
+                .fillMaxHeight(),
+            contentScale = ContentScale.FillBounds,
+            colorFilter = ColorFilter.tint(
+                MaterialTheme.colorScheme.onBackground,
+                blendMode = BlendMode.SrcIn
+            )
+        )
+    }
 }
--- app/src/main/java/net/jacobsma/seeingsound/acoustics/composables/Mass.kt
+++ app/src/main/java/net/jacobsma/seeingsound/acoustics/composables/Mass.kt
@@ -1,16 +1,22 @@
 package net.jacobsma.seeingsound.acoustics.composables
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import net.jacobsma.seeingsound.R
 import net.jacobsma.seeingsound.acoustics.mass.EffectiveMass
 
 @Composable
@@ -18,8 +24,16 @@ fun Mass(mass: EffectiveMass, amplitude: Dp, staticDisplacement: Dp) {
     val massObserver: Number by mass.value.observeAsState(0.0)
     Box(
         modifier = Modifier
-            .size(10.dp * massObserver.toFloat() + 60.dp)
-            .offset(0.dp, staticDisplacement + amplitude)
-            .background(color = Color.Blue) // This must come after setting offset, otherwise offset is not applied
-    )
+            .offset(0.dp, staticDisplacement + amplitude),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(R.drawable.mass),
+            contentDescription = "mass",
+            modifier = Modifier
+                .size(10.dp * massObserver.toFloat() + 60.dp),
+            contentScale = ContentScale.FillBounds,
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onBackground, blendMode = BlendMode.SrcIn)
+        )
+    }
 }
